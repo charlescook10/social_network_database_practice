7091a84367d5ac4184677e94771e9d809e06c773
Charles Cook <charles.cook80@yahoo.co.uk>
2025-10-21 12:20:19 +0100

    Setup

---
+++ app.py
@@ -0,0 +1,31 @@
+from lib.database_connection import DatabaseConnection
+from lib.artist_repository import ArtistRepository
+from lib.album_repository import AlbumRepository
+
+
+# Connect to the database
+connection = DatabaseConnection()
+connection.connect()
+
+# Seed with some seed data
+connection.seed("seeds/music_library.sql")
+
+# Retrieve all artists
+artist_repository = ArtistRepository(connection)
+artists = artist_repository.all()
+
+# Retrieve all albums
+album_repository = AlbumRepository(connection)
+albums = album_repository.all()
+
+# Retrieve a single album
+album_with_id_5 = album_repository.find(5)
+
+# # List them out
+# for artist in artists:
+#     print(artist)
+
+for album in albums:
+    print(album)
+
+print(album_with_id_5)
---
+++ lib/album_repository.py
@@ -0,0 +1,22 @@
+from lib.album import Album
+
+class AlbumRepository:
+    # Initialise with a database connection
+    def __init__(self, connection):
+        self._connection = connection
+
+    # Retrieve all albums
+    def all(self):
+        rows = self._connection.execute('SELECT * from albums')
+        albums = []
+        for row in rows:
+            item = Album(row["id"], row["title"], row["release_year"], row["artist_id"])
+            albums.append(item)
+        return albums
+    
+    # Find a single artist by their id
+    def find(self, album_id):
+        rows = self._connection.execute(
+            'SELECT * from albums WHERE id = %s', [album_id])
+        row = rows[0]
+        return Album(row["id"], row["title"], row["release_year"], row["artist_id"])
